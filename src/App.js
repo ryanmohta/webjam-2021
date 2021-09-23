796930f014c60457c954e7693e1438afdf886ee1
Ryan Mohta <ryanmohta@gmail.com>
2021-09-22 19:58:02 -0700

    Added question component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import coverDark from './images/cover-dark.svg';
 import coverLight from './images/cover-light.svg';
 import './App.scss';
@@ -21,7 +22,15 @@ function App() {
           <Event name="Kickoff Ceremony" shortDate="11" info="Monday, Oct 11 • ICSSC" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
         </div>
       </div>
+      <div className="faq">
+        <h2>FAQs and Rules</h2>
+        <div className="questions">
+          <Question question="Who?" answer="Me!" />
+        </div>
+      </div>
+
     </div>
+
   );
 }
 
@@ -40,6 +49,25 @@ function Event(props) {
       <p>{ props.description }</p>
     </div>
   )
+}
+
+class Question extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { answered: false };
+  }
+
+  handleClick = () => this.setState({ answered: !this.state.answered });
+
+  render() {
+    return (
+      <div className="Question" onClick={this.handleClick}>
+        { this.state.answered ? <p>{ this.props.answer }</p>
+          : <h3>{ this.props.question }</h3>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
